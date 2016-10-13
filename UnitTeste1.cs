47b97e6e645fe6f411004e9ba72291871f4d1961
Marcos Alexandre Fernandes Ferronato <Marcos Alexandre Fernandes Ferronato>
2016-10-13 18:57:41 -0300

    Classe de Testes

---
+++ UnitTeste1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OcrTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Programa capt = new Programa("'0';'1'")
+        }
+    }
+}
