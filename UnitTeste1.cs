839cbe1ee7ce31d49a3cd4efab26a769b9a0e5fb
Marcos Alexandre Fernandes Ferronato <Marcos Alexandre Fernandes Ferronato>
2016-10-14 16:10:06 -0300

    Alteração na classe de teste

--- UnitTeste1.cs
+++ UnitTeste1.cs
@@ -9,7 +9,8 @@ namespace OcrTeste
         [TestMethod]
         public void TestMethod1()
         {
-            Programa capt = new Programa("'0';'1'")
+            Programa capt = new Programa;
+			capt.main("'1';'2'");
         }
     }
 }
